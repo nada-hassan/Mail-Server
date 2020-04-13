218d68204796f1581ec65b066b662f6e7072ab02
nada-hassan <60989613+nada-hassan@users.noreply.github.com>
2020-04-13 18:40:57 +0200

    Add files via upload

---
+++ IPriorityQueue.java
@@ -0,0 +1,5 @@
+package eg.edu.alexu.csd.datastructure.mailServer.cs76_88;
+
+interface IPriorityQueue {
+
+}
---
+++ PriorityQueue.java
@@ -0,0 +1,9 @@
+package eg.edu.alexu.csd.datastructure.mailServer.cs76_88;
+
+class PriorityQueue {
+
+	public PriorityQueue() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
