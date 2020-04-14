b04c385c17136ad6119d31bcedd7f6d5c43ab0dd
nada-hassan <60989613+nada-hassan@users.noreply.github.com>
2020-04-14 02:40:34 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,5 +1,32 @@
 package eg.edu.alexu.csd.datastructure.mailServer.cs76_88;
 
-class Node {
+public class Node {
+int key;
+Object element;
+Node next;
 
+
+public Node(int k, Object o, Node n) {
+	key=k;
+	element=o;
+	next=n;
+}
+public Object getelement() {
+	return element;
+}
+public Node getnext() {
+	return next;
+}
+public int getkey() {
+	return key;
+}
+public void setelement(Object e) {
+	element =e;
+}
+public void setnext(Node n) {
+	next=n;
+}
+public void setkey(int k) {
+	key=k;
+}
 }
